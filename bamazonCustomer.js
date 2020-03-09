0b0be382ec3d4f727a4edb6443172c4f29713a0b
Sharon Patten <EpikNohVuh@gmail.com>
2020-03-09 08:46:05 -0400

    mysqldatabase UPDATE is working, is issuing receipt, is giving InsufficientQuantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,8 +5,8 @@ const Table = require('cli-table');
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "Sharon",
-  password: "pelouzeK5!",
+  user: "",
+  password: "!",
   database: "BAMAZON_DB"
 });
 
@@ -34,16 +34,65 @@ function displayProducts() {
 
     console.log(table.toString());
 
-    inquirer.prompt([{
-      name: 'product',
-      type: 'input',
-      message: "What is the ID of the item you would like to purchase?"
-    },
-    {
-      name: "quantity",
-      type: 'input',
-      message: "How many would you like to purchase?"
-    }]);
+    inquirer.prompt(
+      [
+        {
+          name: 'product',
+          type: 'input',
+          message: "What is the ID of the item you would like to purchase?"
+        },
+        {
+          name: "quantity",
+          type: 'input',
+          message: "How many would you like to purchase?"
+        }
+      ]).then(function (answer) {
+        if (err) throw err;
+        var product = answer.product;
+        var quantity = answer.quantity;
+
+        connection.query('SELECT * FROM products WHERE ?', [{ PRODUCTID: answer.product }], function (err, res) {
+          if (err) throw err;
+
+          if (res[0].STOCK >= answer.quantity) {
+            var subTotal = answer.quantity * res[0].PRICE;
+            console.log(res[0].STOCK);
+            console.log("* * * * * B A M A Z O N   R E C E I P T * * * * *");
+            console.log("You purchased " + answer.quantity + " " + res[0].PRODUCT_NAME + " @ " + res[0].PRICE + " each ");
+            console.log("Your total is: " + subTotal)
+            console.log("* * * * T H A N K  Y O U,  C O M E  A G A I N  * * * ")
+            var updatedStock = res[0].STOCK - quantity;
+            console.log(updatedStock);
+            var query = connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  STOCK: updatedStock
+                },
+                {
+                  PRODUCTID: answer.product
+                }
+              ]);
+
+            
+          } else {
+            console.log("Insufficient Quantity. Please adjust your order, we only have " + res[0].STOCK + " of " + res[0].PRODUCT_NAME + ".");
+
+
+
+          };
+
+        }
+
+
+
+
+
+
+        )
+
+
+      })
 
 
 
@@ -51,4 +100,3 @@ function displayProducts() {
 
 
 };
-
